fb00e80d3fded4eec298ab9237a5af2a00279bef
AMMAR AL HADDAD <91353209+ammaralhaddad@users.noreply.github.com>
2021-10-28 14:57:56 -0400

    Create findWaldoAnonymously.js

---
+++ findWaldoAnonymously.js
@@ -0,0 +1,18 @@
+const findWaldo = function(names, found) {
+  for (let i = 0; i < names.length; i++) {
+    let name = names[i];
+    if (name === "Waldo") {
+      found(i);   // execute callback
+    }
+  }
+}
+
+  
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], function(index){
+
+  console.log('find Walso at index '+" " +index+"!")
+
+
+
+})
